444f033eb411a8cba44407d9d46de4f11a006976
Espadaclar <eneroborde8002@gmail.com>
2017-04-27 12:46:20 +0200

    codificada clase Barco y clase Persona.

---
+++ Barco.java
@@ -0,0 +1,36 @@
+
+/**
+ * franciscoJavier
+ */
+public abstract class  Barco {
+    private Persona persona;
+    private String matricula;
+    private double eslora;
+    private int anio;
+    
+    public Barco(String matricula, double eslora, int anio, Persona persona) {
+        this.persona = persona;
+        this.matricula = matricula;
+        this.anio = anio;
+        this.eslora = eslora;
+    }
+
+    public double getEslora() {
+        return eslora;
+    }
+    
+    public abstract int getCoeficienteBernua();
+
+    @Override
+    public String toString() {
+        String datos = "";
+        datos += "Nombre propietario: " +persona.getName()+ "\n";
+        datos += "Matrícula del barco: " +matricula+ "\n";
+        datos += "Metros de eslora: " +eslora+ " m.\n";
+        datos += "Año de fabricación: " +anio;
+        return datos;
+        
+        
+    }
+       
+}
