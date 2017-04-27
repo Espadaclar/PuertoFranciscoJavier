f1ddd6a38f194b0c991abf86522bea065acaf12f
LuisJCG <Lujacur@gmail.com>
2017-04-27 23:02:05 +0200

    Añadidos comentarios javadoc clase Alquiler

--- Alquiler.java
+++ Alquiler.java
@@ -1,16 +1,32 @@
+
+/**
+ * @Author Luis Javier Curbeira
+ *
+ * Define un Alquiler para unos dias, un barco y un hueco
+ * 
+ */
+
 public class Alquiler {
 	private int diasOcupacion;
 	private Barco barco;
 	private int posicionAmarre;
 	private static int  MULTIPLICADOR_ESLORA = 10;
 	private static int  MULTIPLICADOR_BERNUA = 300;
-
+	
+	
+	/**
+ 	* Define un Alquiler para unos dias, un barco y un hueco
+ 	*/
 	public Alquiler(int diasOcupacion, Barco barco, int posicionAmarre) {
 		this.diasOcupacion = diasOcupacion;
 		this.barco = barco;
 		this.posicionAmarre = posicionAmarre;
 	}
-	
+
+	/**
+ 	* Obtiene el precio del alquiler
+	* @return precio del alquiler
+ 	*/
 	public float getPrecioAlquiler() {
 		float precioAlquiler;
 		precioAlquiler = (diasOcupacion * MULTIPLICADOR_ESLORA * (float)barco.getEslora())
@@ -18,14 +34,17 @@ public class Alquiler {
 		return precioAlquiler;
 	}
 	
+	/**
+ 	* Obitiene el amarre que ocupa un alquiler
+	* @return Posicion del amarre
+ 	*/
 	public int getNumeroAmarre(){
 		return posicionAmarre;
 	}
 	
-	public String toString() 
-    {
-        String valorADevolver = "";
-        valorADevolver = barco  + "Dias ocupacion: "+ diasOcupacion + "\n" + "Posicion de amarre " + posicionAmarre + "\n";
-        return valorADevolver;
+	public String toString() {
+		String valorADevolver = "";
+		valorADevolver = barco  + "Dias ocupacion: "+ diasOcupacion + "\n" + "Posicion de amarre " + posicionAmarre + "\n";
+		return valorADevolver;
     }
 }
